f126be9e8543d45ae1aea71084ef885cec26499d
DutchFakeTuber <sybrandvanloenen@hotmail.com>
2021-12-06 22:40:35 +0100

    Update Day 5

--- 2021/Day 5/Day 5 - Hydrothermal Venture.py
+++ 2021/Day 5/Day 5 - Hydrothermal Venture.py
@@ -17,12 +17,7 @@ def partOne(data: list, grid: list) -> int:
                 yStart, yEnd = yEnd, yStart
             grid[xStart, yStart:yEnd + 1] += 1
     
-    overlap: int = 0
-    for line in grid:
-        for column in line:
-            if column > 1:
-                overlap += 1
-    return overlap
+    return numpy.count_nonzero(grid > 1)
 
 def partTwo(data: list, grid: list) -> int:
     for row in data:
@@ -36,19 +31,21 @@ def partTwo(data: list, grid: list) -> int:
                 yStart, yEnd = yEnd, yStart
             grid[xStart, yStart:yEnd + 1] += 1
         else:
-            if xStart > xEnd:
-                xStart, xEnd = xEnd, xStart
-            if yStart > yEnd:
-                yStart, yEnd = yEnd, yStart
-            print(f"xStart: {xStart}, xEnd: {xEnd}; yStart: {yStart}, yEnd: {yEnd}, length: {xEnd-xStart}, {yEnd-yStart}; range len: {len([x for x in range(0, (yEnd-yStart)+1)])}")
-            for Axis in range(0, yEnd - yStart):
-                grid[xStart+Axis,yStart+Axis] += 1
+            if xStart > xEnd: xStart += 1
+            else: xEnd += 1
+            if yStart > yEnd: yStart += 1
+            else: yEnd += 1
+
+            xAxis = [x for x in range(xStart, xEnd, 1 if xStart < xEnd else -1)]
+            yAxis = [y for y in range(yStart, yEnd, 1 if yStart < yEnd else -1)]
+            for x, y in zip(xAxis, yAxis):
+                grid[x ,y] += 1
 
     return numpy.count_nonzero(grid > 1)
 
 def main() -> None:
     data: list = getData(DATA)
-    print(partOne(data, numpy.zeros((1000, 1000))))
+    # print(partOne(data, numpy.zeros((1000, 1000))))
     print(partTwo(data, numpy.zeros((1000, 1000))))
 
 if __name__ == "__main__":
