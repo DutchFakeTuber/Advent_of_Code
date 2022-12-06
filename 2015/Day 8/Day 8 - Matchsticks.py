7cac5a005a2c977c1f947acca62e56d2802cad5d
DutchFakeTuber <sybrandvanloenen@hotmail.com>
2022-12-06 09:47:03 +0100

    Day 8 of 2015 completed

--- 2015/Day 8/Day 8 - Matchsticks.py
+++ 2015/Day 8/Day 8 - Matchsticks.py
@@ -7,7 +7,8 @@ def partOne() -> int:
     return sum([len(line) - len(ast.literal_eval(line)) for line in getData()])
 
 def partTwo() -> int:
-    ...
+    func: function = lambda x: x.replace('\\', '\\\\').replace('"', '\\"')
+    return sum([len(f'"{func(line)}"') - len(line) for line in getData()])
 
 def main() -> None:
     print(partOne())
@@ -15,4 +16,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
