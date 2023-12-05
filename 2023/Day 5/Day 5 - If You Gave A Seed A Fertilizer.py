7d8716f92a005e7a31a76280286bdf6bf69f49ab
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2023-12-05 22:24:03 +0100

    Still busy with part two

--- 2023/Day 5/Day 5 - If You Gave A Seed A Fertilizer.py
+++ 2023/Day 5/Day 5 - If You Gave A Seed A Fertilizer.py
@@ -4,7 +4,8 @@ class Almanac:
     def __init__(self, data: str) -> None:
         self.seeds: list = [int(seed) for seed in data.splitlines()[0].strip('seeds: ').split(' ')]
         self.data: list = [line for line in data.splitlines()[3:] if len(line) > 0]
-        self.locations: dict[int] = {seed: [] for seed in self.seeds}
+        self.location: int = 10**50
+        self.found: bool = False
     
     def getMaps(self) -> list[list[int]]:
         self.maps: dict = {}
@@ -15,33 +16,83 @@ class Almanac:
             if len(self.data) and not self.data[0].isnumeric():
                 self.data = self.data[1:]
 
-    def checkMaps(self, seed: int, location: int, destination: int=0) -> None:
+    def checkMaps(self, location: int, destination: int=0) -> None:
+        if self.found: return
         recursion: bool = destination < len(self.maps) - 1
         present: bool = False
         for d, s, r in self.maps[destination]:
             if s <= location <= s+r:
                 present = True
                 if recursion:
-                    self.checkMaps(seed, d+(location-s), destination=destination+1)
-                else:
-                    self.locations[seed].append(d+(location-s))
-                    print(seed, d+(location-s))
+                    self.checkMaps(d+(location-s), destination=destination+1)
+                elif not self.found:
+                    self.location = min([self.location, d+(location-s)])
+                    self.found = True
                     return
         if not present:
             if recursion:
-                self.checkMaps(seed, location, destination=destination+1)
-            else:
-                self.locations[seed].append(location)
-                print(seed, location)
+                self.checkMaps(location, destination=destination+1)
+            elif not self.found:
+                self.location = min([self.location, location])
+                self.found = True
                 return
+    
+    def partTwo(self, location: list[list[int]], destination=0) -> int:
+        recursion: bool = destination < len(self.maps) - 1
+        currMap: list = location
+        nextMap: list = []
+        for d, s, r in self.maps[destination]:
+            position: int = 0
+            while position < len(currMap):
+                if currMap[position][-1] < s: pass
+                elif currMap[position][0] > s+r: pass
+                else:
+                    ##### BETWEEN THESE LINES SHOULD BE ENHANCED
+                    curr: set = set(range(currMap[position][0], currMap[position][-1]+1))
+                    dest: set = set(range(s, s+r+1))
+                    intersect: set = dest.intersection(curr)
+                    intersect: list[int, int] = [min(intersect), max(intersect)]
+                    ##### BETWEEN THESE LINES SHOULD BE ENHANCED
+                    nextMap.append(list([d+(intersect[0]-s), d+(intersect[1]-s)]))
+                    if currMap[position][0] < intersect[0]:
+                        currMap.append([currMap[position][0], intersect[0]-1])
+                    if currMap[position][1] > intersect[1]:
+                        currMap.append([currMap[position][0], intersect[1]+1])
+                    currMap = [*currMap[:position], *currMap[position+1:]]
+                position += 1
+        if destination < len(self.maps):
+            nextMap = [*nextMap, *currMap]
+        if recursion:
+            self.partTwo(nextMap, destination=destination+1)
+        if not self.found:
+            self.location = min([self.location, min([min(nums) for nums in nextMap])])
+            self.found = True
 
     def process(self) -> int:
-        self.getMaps()
         for seed in self.seeds:
-            self.checkMaps(seed=seed, location=seed)
+            self.found = False
+            self.checkMaps(location=seed)
+        return self.location
+
+def partOne(data: str) -> int:
+    almanac: Almanac = Almanac(data)
+    almanac.getMaps()
+    seeds: list = almanac.seeds
+    for seed in range(len(seeds)):
+        almanac.seeds = [seeds[seed]]
+        almanac.process()
+    return almanac.location
+
+def partTwo(data: str):
+    almanac: Almanac = Almanac(data)
+    almanac.getMaps()
+    seeds: list = almanac.seeds
+    for seed in range(0, len(seeds), 2):
+        almanac.found = False
+        almanac.seeds = [[seeds[seed], seeds[seed]+seeds[seed+1]]]
+        almanac.partTwo(almanac.seeds)
+    return almanac.location
 
 if __name__ == "__main__":
-    almanac: Almanac = Almanac(TEST)
-    almanac.process()
-    print(almanac.locations)
-    # print(*almanac.maps.values(), sep='\n')
+    print(partOne(DATA))
+    print(partTwo(DATA))
