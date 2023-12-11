93c89f9e6eb97f7a6defde47fdd43769d2bcad6c
DutchFakeTuber <sybrandvanloenen@hotmail.com>
2023-12-11 14:13:49 +0100

    Completed part one

--- 2023/Day 11/Day 11 - Cosmic Expansion.py
+++ 2023/Day 11/Day 11 - Cosmic Expansion.py
@@ -7,8 +7,9 @@ class Space:
     def __init__(self, data: list[list[str]]) -> None:
         self.data: list[list[str]] = data
         self.coords: list[list[int]] = []
-        
+
     def expand(self, row: bool=True) -> None:
+        # For part two: expand coordinates instead of the whole map.
         counter: int = 0
         while counter < (len(self.data) if row else len(self.data[0])):
             empty: bool = all([True if char == '.' else False for char in (self.data[counter] if row else [line[counter] for line in self.data])])
@@ -22,20 +23,28 @@ class Space:
         if row:
             self.expand(row=False)
         self.space: list[list[str]] = self.data
-    
+
+    def _manhattanDistance(self) -> None:
+        self.mandist: set = set()
+        for num, c in enumerate(self.coords):
+            for _num, _c in enumerate(self.coords):
+                if num == _num: continue
+                self.mandist.add(tuple([c if num < _num else _c, _c if num < _num else c, abs(c[0]-_c[0]) + abs(c[1]-_c[1])]))
+        self.mandist = list(self.mandist)
+
     def coordinates(self) -> None:
-        self.coords: list[list[int]] = [[row, col] for row in range(len(self.space)) for col, char in enumerate(self.space[row]) if char == '#']
+        self.coords: list[list[int]] = [tuple([row, col]) for row in range(len(self.space)) for col, char in enumerate(self.space[row]) if char == '#']
+        self._manhattanDistance()
 
 def partOne(data: list[list[str]]) -> int:
     space: Space = Space(data)
     space.expand()
     space.coordinates()
-    print(space.coords)
-    print(len(space.space), len(space.space[0]))
+    return sum(length[2] for length in space.mandist)
 
 def partTwo(data: list[list[str]]) -> int: ...
 
 if __name__ == "__main__":
-    data: list[list[str]] = fetchData(TEST)
+    data: list[list[str]] = fetchData(DATA)
     print(partOne(data))
     print(partTwo(data))
