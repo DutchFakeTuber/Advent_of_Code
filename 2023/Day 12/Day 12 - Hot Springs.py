282e040e97c858f4b6501e5e42a9591176b9ab4f
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2023-12-12 19:54:55 +0100

    Day 12 completed

--- 2023/Day 12/Day 12 - Hot Springs.py
+++ 2023/Day 12/Day 12 - Hot Springs.py
@@ -1,50 +1,48 @@
-from Hot_Springs import TEST, TESTCASE, DATA
-from functools import lru_cache
-from time import perf_counter
+from Hot_Springs import TEST, DATA
+from functools import cache
+from multiprocessing import Pool
 
 def fetchData(data: str) -> list[list[str, tuple[int]]]:
-    return [[line.split()[0], eval(f"[{line.split()[1]}]")] for line in data.splitlines()]
+    return [[line.split()[0], eval(f"({line.split()[1]})")] for line in data.splitlines()]
 
-class HotSpring:
-    def __init__(self, data: list[str, tuple[int]]) -> None:
-        self.data = data
-        self.count: int = 0
-        self.arrangements: int = 0
-        self.match: list = []
+@cache
+def pattern(patt: str, clues: tuple[int], size: int=0) -> None:
+    if not patt:
+        if ((len(clues) == 1 and clues[0] == size) or (len(clues) == 0 and size == 0)):
+            return 1
+        return 0
+    curr = patt[0]
+    patt = patt[1:]
+    clue, *new_clues = clues or [0]
+    new_clues = tuple(new_clues)
+    if curr == '?':
+        return pattern('.' + patt, clues, size) + pattern('#' + patt, clues, size)
+    if curr == '#':
+        return 0 if size > clue else pattern(patt, clues, size + 1)
+    if curr == '.':
+        if size == 0:
+            return pattern(patt, clues, 0)
+        elif size == clue:
+            return pattern(patt, new_clues, 0)
+        return 0
 
-    lru_cache(maxsize=None)
-    def pattern(self, patt: str, pos: int, prev: str) -> None:
-        if pos == len(patt):
-            if self.match == [len(chars) for chars in patt.split('.') if chars != '']:
-                self.count += 1
-            return
-        if prev == '#' and patt[pos] == '.':
-            if self.match[0] != [len(chars) for chars in patt.split('.') if chars != ''][0]:
-                return
-        if patt[pos] == '?':
-            self.pattern(patt[:pos] + '.' + patt[pos+1:], pos+1, '.')
-            self.pattern(patt[:pos] + '#' + patt[pos+1:], pos+1, '#')
-        else:
-            self.pattern(patt, pos+1, patt[pos])
-
-    def process(self) -> int:
-        for chars, match in self.data:
-            self.count: int = 0
-            self.match: list = match
-            self.pattern(chars, 0, chars[0])
-            self.arrangements += self.count
+def process(data: list[str, tuple[int]]) -> int:
+    return pattern(patt=data[0], clues=data[1], size=0)
 
 def partOne(data: list[list[list[str], list[int]]]) -> int:
-    hotSpring: HotSpring = HotSpring(data)
-    hotSpring.process()
-    return hotSpring.arrangements
+    with Pool(processes=None) as pool:
+        arrangements: map = pool.map_async(process, data)
+        arrangements: list[int] = arrangements.get()
+    return sum(arrangements)
 
 def partTwo(data: list[list[list[str], list[int]]]) -> int:
-    ...
+    data: list[list[str, list[int]]] = [['?'.join([chars]*5), match*5] for chars, match in data]
+    with Pool(processes=None) as pool:
+        arrangements: map = pool.map_async(process, data)
+        arrangements: list[int] = arrangements.get()
+    return sum(arrangements)
 
 if __name__ == "__main__":
     data: list[list[list[str], list[int]]] = fetchData(DATA)
-    start = perf_counter()
     print(partOne(data))
-    print(perf_counter() - start)
     print(partTwo(data))
--- 2023/README.md
+++ 2023/README.md
@@ -3,7 +3,7 @@ This is my input regarding the Advent of Code 2023 challenge.
 
 | Days Completed | Parts Completed | Stars Accumulated |
 | :------------: | :-------------: | :---------------: |
-| 11/25          | 22/50           | :star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star::star::star::star::star::star::star:<br>:star::star: |
+| 12/25          | 24/50           | :star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star: |
 
 | Day                                               | Puzzle Input File         | Solution File           | Part One | Part Two | Total Stars   |
 | ------------------------------------------------- | :------------------------ | :---------------------- | :------: | :------: | :-----------: |
@@ -18,7 +18,7 @@ This is my input regarding the Advent of Code 2023 challenge.
 | [Day 9 - Mirage Maintenance][DAY_9]               | [Puzzle Input][PUZZLE_9]  | [Solution][SOLUTION_9]  |  :star:  |  :star:  | :star::star:  |
 | [Day 10 - Pipe Maze][DAY_10]                      | [Puzzle Input][PUZZLE_10] | [Solution][SOLUTION_10] |  :star:  |  :star:  | :star::star:  |
 | [Day 11 - Cosmic Expansion][DAY_11]               | [Puzzle Input][PUZZLE_11] | [Solution][SOLUTION_11] |  :star:  |  :star:  | :star::star:  |
-| [Day 12 - ][DAY_12]                               | [Puzzle Input][PUZZLE_12] | [Solution][SOLUTION_12] |    |    |   |
+| [Day 12 - Hot Springs][DAY_12]                    | [Puzzle Input][PUZZLE_12] | [Solution][SOLUTION_12] |  :star:  |  :star:  | :star::star:  |
 | [Day 13 - ][DAY_13]                               | [Puzzle Input][PUZZLE_13] | [Solution][SOLUTION_13] |    |    |   |
 | [Day 14 - ][DAY_14]                               | [Puzzle Input][PUZZLE_14] | [Solution][SOLUTION_14] |    |    |   |
 | [Day 15 - ][DAY_15]                               | [Puzzle Input][PUZZLE_15] | [Solution][SOLUTION_15] |    |    |   |
@@ -72,7 +72,7 @@ This is my input regarding the Advent of Code 2023 challenge.
 [SOLUTION_9]:  Day%209/Day%209%20-%20Mirage%20Maintenance.py
 [SOLUTION_10]: Day%2010/Day%2010%20-%20Pipe%20Maze.py
 [SOLUTION_11]: Day%2011/Day%2011%20-%20Cosmic%20Expansion.py
-[SOLUTION_12]: Day%2012/Day%2012%20-%20
+[SOLUTION_12]: Day%2012/Day%2012%20-%20Hot%20Springs.py
 [SOLUTION_13]: Day%2013/Day%2013%20-%20
 [SOLUTION_14]: Day%2014/Day%2014%20-%20
 [SOLUTION_15]: Day%2015/Day%2015%20-%20
@@ -99,7 +99,7 @@ This is my input regarding the Advent of Code 2023 challenge.
 [PUZZLE_9]:  Day%209/Mirage_Maintenance.py
 [PUZZLE_10]: Day%2010/Pipe_Maze.py
 [PUZZLE_11]: Day%2011/Cosmic_Expansion.py
-[PUZZLE_12]: Day%2012/
+[PUZZLE_12]: Day%2012/Hot_Springs.py
 [PUZZLE_13]: Day%2013/
 [PUZZLE_14]: Day%2014/
 [PUZZLE_15]: Day%2015/
