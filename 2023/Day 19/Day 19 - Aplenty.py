204927009cb3a91e38b26f708448acc3b55ea5fc
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2023-12-19 20:25:28 +0100

    Completed day 19

--- 2023/Day 19/Day 19 - Aplenty.py
+++ 2023/Day 19/Day 19 - Aplenty.py
@@ -6,19 +6,60 @@ def fetchData(data: str) -> tuple[dict, list[dict]]:
     ratings: list[dict] = [{value[0]: int(value[2:]) for value in line[1:-1].split(',')} for line in data.splitlines()[split+1:]]
     return workflow, ratings
 
-def check(r: dict, work: dict, character: str='in') -> str:
+class Node:
+    def __init__(self,
+                 x: range=range(1, 4001),
+                 m: range=range(1, 4001),
+                 a: range=range(1, 4001),
+                 s: range=range(1, 4001)) -> None:
+        self.x, self.m, self.a, self.s = x, m, a, s
+    
+    def __len__(self) -> int:
+        return len(self.x)*len(self.m)*len(self.a)*len(self.s)
+    
+    def copy(self) -> object:
+        return Node(self.x, self.m, self.a, self.s)
+    
+    def process(self, xmas: str, operation: str, value: int) -> None:
+        left: Node = self.copy()
+        right: Node = self.copy()
+        rating: range = getattr(self, xmas)
+        if operation == '>':
+            l: range = range(value+1, rating.stop)
+            r: range = range(rating.start, value+1)
+        elif operation == '<':
+            l: range = range(rating.start, value)
+            r: range = range(value, rating.stop)
+        setattr(left, xmas, l)
+        setattr(right, xmas, r)
+        return left, right
+
+def checkNodes(workflow: dict, work: str, node: Node) -> int:
+    if work == 'A':
+        return len(node)
+    elif work == 'R':
+        return 0
+    total: int = 0
+    *conditions, default = workflow[work]
+    for cond in conditions:
+        cond, target = cond.split(':')
+        lnode, node = node.process(cond[0], cond[1], int(cond[2:]))
+        total += checkNodes(workflow, target, lnode)
+    return total + checkNodes(workflow, default, node)
+
+def check(ratings: list[dict], workflow: dict, work: str='in') -> str:
     parse: function = lambda x: (x[0], x[1], int(x[2:].split(':')[0]), x[2:].split(':')[1])
-    while character not in ['A', 'R']:
-        for w in work[character]:
+    while work not in ['A', 'R']:
+        for w in workflow[work]:
             if '<' in w or '>' in w:
                 char, inst, num, move = parse(w)
-                if (r[char] < num) if inst == '<' else (r[char] > num):
-                    character = move
+                if (ratings[char] < num) if inst == '<' else (ratings[char] > num):
+                    work = move
                     break
             else:
-                character = w
+                work = w
                 break
-    return character
+    return work
 
 def partOne(workflow: dict, ratings: list[dict]) -> int:
     accepted: list = []
@@ -28,10 +69,9 @@ def partOne(workflow: dict, ratings: list[dict]) -> int:
     return sum([sum(a.values()) for a in accepted])
 
 def partTwo(workflow: dict, ratings: list[dict]) -> int:
-    # Make a https://en.wikipedia.org/wiki/K-d_tree!
-    ...
+    return checkNodes(workflow, 'in', Node())
 
 if __name__ == "__main__":
-    workflow, ratings = fetchData(TEST)
+    workflow, ratings = fetchData(DATA)
     print(partOne(workflow, ratings))
     print(partTwo(workflow, ratings))
--- 2023/README.md
+++ 2023/README.md
@@ -5,7 +5,7 @@ This is my input regarding the Advent of Code 2023 challenge.
 
 | Days Completed | Parts Completed | Stars Accumulated |
 | :------------: | :-------------: | :---------------: |
-| 18/25          | 36/50           | :star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star::star::star: |
+| 19/25          | 38/50           | :star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star::star::star::star::star::star::star:<br>:star::star::star::star::star::star::star::star: |
 
 | Day                                               | Puzzle Input File         | Solution File           | Part One | Part Two | Total Stars   |
 | ------------------------------------------------- | :------------------------ | :---------------------- | :------: | :------: | :-----------: |
@@ -27,7 +27,7 @@ This is my input regarding the Advent of Code 2023 challenge.
 | [Day 16 - The Floor Will Be Lava][DAY_16]         | [Puzzle Input][PUZZLE_16] | [Solution][SOLUTION_16] |  :star:  |  :star:  | :star::star:  |
 | [Day 17 - Clumsy Crucible][DAY_17]                | [Puzzle Input][PUZZLE_17] | [Solution][SOLUTION_17] |  :star:  |  :star:  | :star::star:  |
 | [Day 18 - Lavaduct Lagoon][DAY_18]                | [Puzzle Input][PUZZLE_18] | [Solution][SOLUTION_18] |  :star:  |  :star:  | :star::star:  |
-| [Day 19 - ][DAY_19]                               | [Puzzle Input][PUZZLE_19] | [Solution][SOLUTION_19] |    |    |   |
+| [Day 19 - Aplenty][DAY_19]                        | [Puzzle Input][PUZZLE_19] | [Solution][SOLUTION_19] |  :star:  |  :star:  | :star::star:  |
 | [Day 20 - ][DAY_20]                               | [Puzzle Input][PUZZLE_20] | [Solution][SOLUTION_20] |    |    |   |
 | [Day 21 - ][DAY_21]                               | [Puzzle Input][PUZZLE_21] | [Solution][SOLUTION_21] |    |    |   |
 | [Day 22 - ][DAY_22]                               | [Puzzle Input][PUZZLE_22] | [Solution][SOLUTION_22] |    |    |   |
@@ -81,7 +81,7 @@ This is my input regarding the Advent of Code 2023 challenge.
 [SOLUTION_16]: Day%2016/Day%2016%20-%20The%20Floor%20Will%20Be%20Lava.py
 [SOLUTION_17]: Day%2017/Day%2017%20-%20Clumsy%20Crucible.py
 [SOLUTION_18]: Day%2018/Day%2018%20-%20Lavaduct%20Lagoon.py
-[SOLUTION_19]: Day%2019/Day%2019%20-%20
+[SOLUTION_19]: Day%2019/Day%2019%20-%20Aplenty.py
 [SOLUTION_20]: Day%2020/Day%2020%20-%20
 [SOLUTION_21]: Day%2022/Day%2021%20-%20
 [SOLUTION_22]: Day%2022/Day%2022%20-%20
@@ -108,7 +108,7 @@ This is my input regarding the Advent of Code 2023 challenge.
 [PUZZLE_16]: Day%2016/The_Floor_Will_Be_Lava.py
 [PUZZLE_17]: Day%2017/Clumsy_Crucible.py
 [PUZZLE_18]: Day%2018/Lavaduct_Lagoon.py
-[PUZZLE_19]: Day%2019/
+[PUZZLE_19]: Day%2019/Aplenty.py
 [PUZZLE_20]: Day%2020/
 [PUZZLE_21]: Day%2021/
 [PUZZLE_22]: Day%2022/
