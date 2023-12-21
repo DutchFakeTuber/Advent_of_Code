d1f3240a1d4914b388606bbf287fc8e391cbc45f
DutchFakeTuber <sybrandvanloenen@hotmail.com>
2023-12-21 09:51:37 +0100

    Day 20 completed

--- 2023/Day 20/Day 20 - Pulse Propagation.py
+++ 2023/Day 20/Day 20 - Pulse Propagation.py
@@ -1,6 +1,5 @@
 from Pulse_Propagation import TEST, DATA
 from dataclasses import dataclass, field
-from time import perf_counter_ns
 
 def fetchData(data: str) -> dict:
     modules: dict = {}
@@ -20,7 +19,6 @@ class FlipFlop:
     def process(self, pulse: bool, module: str) -> bool | None:
         if pulse == False:
             self.state = not self.state
-            print(self.state, pulse)
             return self.state
         return None
 
@@ -31,7 +29,6 @@ class Conjunction:
     type: str = field(init=False, default='Conjunction')
 
     def addMember(self, module) -> None:
-        print(module, self.name)
         if module != self.name:
             self.remember[module] = False
 
@@ -66,37 +63,75 @@ def build(modules: dict) -> dict:
                 dcModules[point].addMember(name)
     return dcModules
 
+def looper(current: list[list[str, bool, str]],
+           modules: dict[str, list[str]],
+           dcModules: dict[str, Conjunction | FlipFlop | Untyped],
+           state: list[str]=[]) -> tuple[dict, dict]:
+    bools: dict[bool, int] = {True: 0, False: 0}
+    pending: list[list[str, bool, str]] = []
+    checkState: bool = bool(len(state))
+    found: list = []
+    while len(current) or len(pending):
+        if not len(current):
+            current = pending
+            pending = []
+        module, true_false, previous = current.pop(0)
+        result: bool = dcModules[module].process(true_false, previous)
+        if result == None:
+            continue
+        if checkState:
+            if dcModules[module].name in state and result:
+                found.append(dcModules[module].name)
+        for point in modules[module]['pointer']:
+            bools[result] += 1
+            pending.append([point, result, module])
+    if checkState:
+        return found, dcModules
+    return bools, dcModules
+
 def partOne(modules: dict) -> int:
     dcModules: dict[str, Conjunction | FlipFlop | Untyped] = build(modules)
     bools: dict[bool, int] = {True: 0, False: 0}
-    # print(*modules.items(), sep='\n')
-    for _ in range(2):
-        print(dcModules)
-        current: list[list[str, bool]] = [['broadcaster', False]]
-        # bools[False] += 1
-        wait: list[list[str, bool]] = []
-        while len(current) or len(wait):
-            if not len(current):
-                current = wait
-                wait = []
-            print(f"{current=}, {wait=}")
-            c, b = current.pop(0)
-            result: bool = dcModules[c].process(b, c)
-            if result == None:
-                continue
-            # print(c, b)
-            bools[result] += 1
-            for point in modules[c]['pointer']:
-                wait.append([point, result])
-    return bools
+    for _ in range(1000):
+        current: list[list[str, bool, str]] = [['broadcaster', False, '']]
+        bools[False] += 1
+        _bools, dcModules = looper(current, modules, dcModules)
+        bools[True] += _bools[True]
+        bools[False] += _bools[False]
+    return bools[True] * bools[False]
 
 def partTwo(modules: dict) -> int:
-    # modules: dict = build(modules)
-    ...
+    """Strategy:
+    
+    Find the Conjunctions previous to `rx`.
+    The layout for the input is as follows:
+    [Conj., Conj., Conj., Conj.] -> Conj. -> `rx`
+    
+    `rx` should receive a low pulse `False`.
+    This equates to: [`True`, `True`, `True`, `True`] -> `False` -> `rx`
+    
+    Returns:
+        int: Conjunctions [Conj., Conj., Conj., Conj.] multiplied.
+    """
+    dcModules: dict[str, Conjunction | FlipFlop | Untyped] = build(modules)
+    previous: str = [k for k, v in modules.items() if 'rx' in v['pointer'] and v['operator'] == '&'][0]
+    previous: list[str] = [k for k, v in modules.items() if previous in v['pointer'] and v['operator'] == '&']
+    counter: int = 0
+    conjunctions: dict = {}
+    while True:
+        counter += 1
+        found, dcModules = looper([['broadcaster', False, '']], modules, dcModules, previous)
+        if len(found):
+            for f in found:
+                if not conjunctions.get(f, False):
+                    conjunctions[f] = counter
+        if len(conjunctions.keys()) == len(previous):
+            result: int = 1
+            for c in conjunctions.values():
+                result *= c
+            return result
 
 if __name__ == "__main__":
-    start = perf_counter_ns()
-    data: dict = fetchData(TEST)
+    data: dict = fetchData(DATA)
     print(partOne(data))
     print(partTwo(data))
-    print(f"{(perf_counter_ns()-start)/1e9}")
