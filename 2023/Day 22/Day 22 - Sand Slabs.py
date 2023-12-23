613ef9f09d7e9c0ba2b6fd340dfc3991ee889b21
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2023-12-23 10:48:32 +0100

    Made some improvements

--- 2023/Day 22/Day 22 - Sand Slabs.py
+++ 2023/Day 22/Day 22 - Sand Slabs.py
@@ -1,25 +1,22 @@
 from Sand_Slabs import TEST, DATA
 from collections import defaultdict
-from time import perf_counter
+from itertools import product
 
 def fetchData(data: str) -> list[list[int]]:
     splitter: function = lambda l: [n for c in l.split('~') for n in c.split(',')]
-    # coordinates: list[list[int]] = []
-    # for line in data.splitlines():
-    #     for 
-    return [list(map(int, splitter(line))) for line in data.splitlines()]
+    return sorted([list(map(int, splitter(line))) for line in data.splitlines()], key=lambda x: x[2])
 
-def drop(coordinates, skip: int=None) -> tuple[bool, int]:
+def drop(coordinates: list[list[int]], skip: int=None) -> tuple[bool, int]:
     peaks: defaultdict = defaultdict(int)
     falls: int = 0
-    
+
     for num, (x1, y1, z1, x2, y2, z2) in enumerate(coordinates):
         # start: (x1, y1, z1), end: (x2, y2, z2)
         if num == skip: continue # If the current row is equal to skip
         # Get (x, y) coordinates as area
-        area: list[tuple[int]] = [(x, y) for x in range(x1, x2+1) for y in range(y1, y2+1)]
+        area: tuple[tuple[int]] = tuple(product(range(x1, x2+1), range(y1, y2+1)))
         # Get tallest peak of the given area. Increment by one.
-        peak: int = max(peaks[a] for a in area) + 1
+        peak: int = max(map(lambda x: peaks[x], area)) + 1
         for xy in area:
             # Rewrite Z-coordinate with its previous peak
             # and offset it with the z2-z1 coordinates.
@@ -31,7 +28,6 @@ def drop(coordinates, skip: int=None) -> tuple[bool, int]:
     return not falls, falls
 
 def parts(coordinates: list[list[int]]) -> int:
-    coordinates.sort(key=lambda c: c[2])
     drop(coordinates) # Let the floating bricks fall
     safe: int = 0
     chain: int = 0
