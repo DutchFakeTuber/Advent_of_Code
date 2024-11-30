ce6f2492e9acece405c85ac62f3ec74c2dfde151
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2024-11-30 12:39:21 +0100

    Preparation for AoC 2024!

---
+++ 2024/Day 21/Day 21 - name.py
@@ -0,0 +1,15 @@
+from name import TEST, DATA
+
+def fetchData(data: str) -> ...:
+    return ...
+
+def partOne(city: ...) -> int:
+    ...
+
+def partTwo(city: ...) -> int:
+    ...
+
+if __name__ == "__main__":
+    data: ... = fetchData(TEST)
+    print(partOne(data))
+    print(partTwo(data))
