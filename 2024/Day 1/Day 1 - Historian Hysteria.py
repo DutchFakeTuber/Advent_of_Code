77fea3d67cc7af2d87398a0f804e8814ab20d00f
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2024-12-01 19:42:10 +0100

    Optimized code

--- 2024/Day 1/Day 1 - Historian Hysteria.py
+++ 2024/Day 1/Day 1 - Historian Hysteria.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from Historian_Hysteria import TEST, DATA
 
 def fetchData(data: str) -> list[list[int, int]]:
@@ -7,7 +8,7 @@ def partOne(data: list[list[int, int]]) -> int:
     return sum([abs(l-r) for l, r in zip(sorted([num[0] for num in data]), sorted([num[1] for num in data]))])
 
 def partTwo(data: list[list[int, int]]) -> int:
-    appearance: dict = {location[0]: sum([1 if location[0] == loc[1] else 0 for loc in data]) for location in data}
+    appearance: Counter = Counter(location[1] for location in data)
     return sum(appearance[location[0]] * location[0] for location in data)
 
 if __name__ == "__main__":
