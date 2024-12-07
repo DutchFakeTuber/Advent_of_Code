101e5a2896b83e87140240a9f8c9010ec2bd4f8c
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2024-12-07 22:52:54 +0100

    Changed template

---
+++ template/Day 20/solution.py
@@ -0,0 +1,16 @@
+TEST: str = open("test.txt")
+DATA: str = open("input.txt")
+
+def fetchData(data: str) -> ...:
+    return ...
+
+def partOne(data: ...) -> int:
+    ...
+
+def partTwo(data: ...) -> int:
+    ...
+
+if __name__ == "__main__":
+    data: ... = fetchData(TEST)
+    print(partOne(data))
+    print(partTwo(data))
