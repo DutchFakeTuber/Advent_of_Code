1fbd3ff59bd169332fb77adf338816d58ba4ba9f
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2024-12-14 18:22:43 +0100

    Updated Template map

--- template/Day 14/solution.py
+++ template/Day 14/solution.py
@@ -1,16 +1,18 @@
-TEST: str = open("test.txt")
-DATA: str = open("input.txt")
+from os.path import dirname, realpath
+
+TEST: str = open(f"{dirname(realpath(__file__))}\\test.txt").read()
+DATA: str = open(f"{dirname(realpath(__file__))}\\input.txt").read()
 
 def fetchData(data: str) -> ...:
-    return ...
+    return 
 
 def partOne(data: ...) -> int:
-    ...
+    return 
 
 def partTwo(data: ...) -> int:
-    ...
+    return 
 
 if __name__ == "__main__":
-    data: ... = fetchData(TEST)
+    data: list[list[int]] = fetchData(DATA)
     print(partOne(data))
     print(partTwo(data))
